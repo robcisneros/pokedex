2135b23a795a433b3545f8b0d593877860b953aa
robcisneros <robertocisneroslp@gmail.com>
2022-03-19 22:17:09 -0600

    types dinamicos

--- pokedex.js
+++ pokedex.js
@@ -2,7 +2,7 @@ const fetchPokemon = () => {
   const pokeNameInput = document.getElementById("pokeName");
   let pokeName = pokeNameInput.value.trim();
   pokeName = pokeName.toLowerCase();
-  console.log(pokeName);
+
   if (pokeName.length <= 0) {
     return;
   } else {
@@ -13,21 +13,63 @@ const fetchPokemon = () => {
           console.log(res);
           pokeImage("./images/pokemon-sad.gif");
         } else {
+          const tipoContainer = document.getElementById("typeContent");
+          tipoContainer.innerHTML = "";
           return res.json();
+
+          
         }
       })
       .then((data) => {
         if (data) {
           console.log(data);
-          let pokeImg = data.sprites.front_default;
-          pokeImage(pokeImg);
-          console.log(pokeImg);
+          let dataName = data.name;
+          let dataImg = data.sprites.front_default;
+          let dataTypes = data.types;
+          addTypeButtons(dataTypes);
+          let dataType = data.types[0].type.name.toUpperCase();
+          let dataHeight = data.height;
+          let dataWeight = data.weight;
+          /*let dataStats = data.stats;
+          console.log(dataStats);*/
+          pokeObject = {
+            name: dataName,
+            image: dataImg,
+            type: dataType,
+            height: dataHeight,
+            weight: dataWeight
+          }
+          pokeData(pokeObject);
+          
         }
       });
   }
 };
 
-const pokeImage = (url) => {
+const pokeImage = (image) => {
   const pokePhoto = document.getElementById("pokeImg");
-  pokePhoto.src = url;
+  pokePhoto.src = image;
 };
+
+const pokeData = (pokeObject) => {
+  const pokePhoto = document.getElementById("pokeImg");
+  pokePhoto.src = pokeObject.image;
+  /*const pokeType = document.getElementById("pokeTipo");
+  pokeType.innerHTML = pokeObject.type;*/
+};
+
+
+const addTypeButtons = (types) => {
+  const tipoContainer = document.getElementById("typeContent");
+  pokeLength = types.length;
+  console.log("array de: " + pokeLength);
+  types.map(typeItem =>{
+    let ntype = typeItem.type.name.toUpperCase();
+    console.log(ntype);
+    const newButton = document.createElement("button");
+    newButton.classList.add("contentData");
+    newButton.innerHTML = ntype;
+    tipoContainer.appendChild(newButton);
+  });
+};
+
